c12410a06e50e098bf0a043a4b59f987580ab28e
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-24 15:24:27 -0800

    feat: :sparkles: introdoce user model

---
+++ server/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: 'string', required: true },
+  lastName: { type: 'string', required: true },
+  email: { type: 'string', required: true, unique: true },
+  password: { type: 'string', required: true },
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  // friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Friends' }],
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+export default User
