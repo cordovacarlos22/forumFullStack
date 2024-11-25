a37e6e81af709f7e1a0ff044dbf68674f1d10b66
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-24 17:03:11 -0800

    feat: :sparkles: add template for routes

---
+++ server/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import * as userController from '../controller/user.auth.controller.js';
+
+import * as userAuth from '../controller/user.auth.controller.js';
+
+const userRoutes = express.Router();
+
+
+export default userRoutes;
