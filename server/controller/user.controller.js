e3d516179f4df6bd6d513552a6244fba9fadbc25
Hector Castro <raidrex@hotmail.com>
2024-11-24 18:17:34 -0600

    🚀 controladores para update y delete de users y agregacion de llaves al schema users

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,7 +1,6 @@
 import User from "../models/user.model";
 
-
-// ! CRUD OPERATIONS 
+// ! CRUD OPERATIONS
 
 const getAllUser = async (res, req) => {
   try {
@@ -10,36 +9,50 @@ const getAllUser = async (res, req) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
 const getUserById = async (res, req) => {
-
   if (!req.params.userId.match(/^[0-9a-fA-F]{24}$/)) {
     return res.status(400).json({ message: "Invalid user ID" });
   }
   try {
-    
-    const user = await User.findById({ id: req.params.userId }, { password: 0 })
+    const user = await User.findById(
+      { id: req.params.userId },
+      { password: 0 }
+    );
     if (!user) return res.status(404).json({ message: "User not found" });
     res.status(200).json(user);
   } catch (error) {
-    res.status(400).json({ message: 'Failed to get user ', error: error });
+    res.status(400).json({ message: "Failed to get user ", error: error });
   }
 };
 
 const updateUser = async (res, req) => {
-
+  if (!req.params.userId.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(400).json({ message: "Invalid user ID" });
+  }
+  const { userId } = req.params.userId
+  const updateData = req.body
+  try {
+    const user = await User.findByIdAndUpdate(userId, updateData, { new: true })
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ message: "Failed to update user ", error: error });
+  }
 };
 
 const deleteUser = async (res, req) => {
-
+  if (!req.params.userId.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(400).json({ message: "Invalid user ID" });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.userId, { isActive: false }, { new: false} )
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.status(204).json();
+  } catch (error) {
+    res.status(400).json({ message: "Failed to delete user ", error: error })
+  }
 };
 
-
-
-export {
-  getAllUser,
-  getUserById,
-  updateUser,
-  deleteUser,
-}
+export { getAllUser, getUserById, updateUser, deleteUser };
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose';
-
+const role = ['user', 'admin']
+const avatarDefault = 'https://www.clinicaariasdent.com/_next/static/media/man.05d37c2e.svg'
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: 'string', required: true },
-  lastName: { type: 'string', required: true },
-  email: { type: 'string', required: true, unique: true },
-  password: { type: 'string', required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: [String], enum: role, defautl: 'user'},
+  avatar: { type: String, default: avatarDefault },
+  bio: { type: String},
+  isActive: {type: Boolean, default: true}
   // posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
   // comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   // friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Friends' }],
