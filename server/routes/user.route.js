db75d3a936188278c83cca665203adf569248280
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-26 12:58:34 -0800

    fix: :bug: fix path for user routes.

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -7,11 +7,11 @@ import { isAdmin } from '../middlewares/isAdmin.js';
 
 const userRoutes = express.Router();
 
-userRoutes.post('/register', userAuth.register); // register user
-userRoutes.post('/login', userAuth.login); // login user 
+userRoutes.post('/user/register', userAuth.register); // register user
+userRoutes.post('/user/login', userAuth.login); // login user 
 userRoutes.get('/user', isAuth, isAdmin, userController.getAllUser); // get all user by admin only 
-userRoutes.get('/user', isAuth,isAdmin, userController.getUserById);// get user by id user most by auth
-userRoutes.param('/user/:userId', isAuth, userController.updateUserById) // update user by id 
+userRoutes.get('/user/:userId', isAuth, isAdmin, userController.getUserById);// get user by id user most by auth
+userRoutes.patch('/user/:userId', isAuth, userController.updateUserById) // update user by id 
 userRoutes.delete('/user/:userId', isAuth,isAdmin, userController.deleteUserById) // update user by id 
 
 export default userRoutes;
