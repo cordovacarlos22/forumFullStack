ca763bb0eb0d6aec14cffa40afc1ea73ffdb63ad
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-25 18:06:44 -0800

    fix: :bug: fix schema title

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
-  title: { type: String, required: true, unique },
+  title: { type: String, required: true, unique:true },
   content: { type: String, required: true },
   image: [{ type: String, required: true }], // For storing the image URL in MongoDB
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
