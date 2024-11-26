78cc3bbba1d8605c6c369a331ced3d00ece44351
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-26 07:01:00 -0800

    feat: :sparkles: add authentication to delete route

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -15,10 +15,10 @@ postRoute.get('/post', postController.getAllPosts);
 postRoute.get('/post/:postId', postController.getPostById);
 
 // update post by id with auth validation middleware
-postRoute.patch('/post/:postId',isAuth,isAdmin,  postController.updatePostById);
+postRoute.patch('/post/:postId',isAuth,  postController.updatePostById);
 
 // delete post by id with auth validation middleware
-postRoute.delete('/post/:postId', postController.deletePostById);
+postRoute.delete('/post/:postId',isAuth, postController.deletePostById);
 
 
 export default postRoute
