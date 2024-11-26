8991b00453b31fddb5c00c8ad44f6f4051b36ec4
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-26 07:57:05 -0800

    feat: :sparkles: add delele by id

--- server/controller/forum.controller.js
+++ server/controller/forum.controller.js
@@ -51,20 +51,28 @@ const updateForumById = async (req, res) => {
   }
   try {
     const forum = await Forum.findByIdAndUpdate(req.params.forumId, req.body, { new: true }).populate("posts", "_id title content image authors ");
-    
+
     if (!forum) return res.status(404).json({ message: 'No Forum Was Found!' });
 
     res.status(200).json(forum);
   } catch (error) {
-
+    res.status(400).json({ message: error.message })
   }
 };
 
 const deleteForumById = async (req, res) => {
+  // check to see if ID is a matching with a matching object(has to be 24  hexadecimal  alphanumeric characters)
+  if (!req.params.forumId.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(400).json({ message: 'Invalid post ID' })
+  }
   try {
+    const forum = await Forum.findByIdAndUpdate(req.params.forumId, { isActive: false }, { new: false });
 
+    if (!forum) return res.status(404).json({ message: 'No Forum Was Found!' });
+
+    res.status(204).end();
   } catch (error) {
-
+    res.status(400).json({ message: error.message })
   }
 };
 
