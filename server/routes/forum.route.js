c7043598bcb620633d5c325032fc9242fac6b89b
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-26 07:59:54 -0800

    feat: :sparkles: add auth for routes

--- server/routes/forum.route.js
+++ server/routes/forum.route.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import * as forumController from '../controller/forum.controller.js'
 const forumRoutes = express.Router();
-
+import { isAdmin } from '../middlewares/isAdmin.js';
+import { isAuth } from '../middlewares/isAuth.js';
 
 
 // create a new forum with title, description and posts array
 forumRoutes.post('/forum', forumController.createForum);
 
 // get all forums
-forumRoutes.get('/forum', forumController.getAllForums);
+forumRoutes.get('/forum', isAuth, isAdmin, forumController.getAllForums);
 
 // get forum by id
 forumRoutes.get('/forum/:forumId', forumController.getForumById);
 
 // update forum by id with auth validation middleware
-forumRoutes.patch('/forum/:forumId', forumController.updateForumById);
+forumRoutes.patch('/forum/:forumId', isAuth, isAuth, forumController.updateForumById);
 
 // delete forum by id with auth validation middleware
-forumRoutes.delete('/forum/:forumId', forumController.deleteForumById);
+forumRoutes.delete('/forum/:forumId', isAuth, isAdmin, forumController.deleteForumById);
 
 export default forumRoutes;
