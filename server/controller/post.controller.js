ad4447e6a5dce46c92cb317f28989d9535efd4e6
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-25 18:01:37 -0800

    feat: :sparkles: add createPost Controller working

---
+++ server/controller/post.controller.js
@@ -0,0 +1,76 @@
+import Post from "../models/post.model.js";
+import s3UploadV3 from "../config/s3Service.js";
+
+// Create a new post
+const createPost = async (req, res) => {
+  
+  if (!req.files) {
+
+    return res.status(400).json({ message: 'No file uploaded' });
+  };
+
+  try {
+    const result = await s3UploadV3(req.files); // function takes files and uploads them to s3 
+
+
+    const post = await Post.create({
+      ...req.body,
+      image: result.urls, // Guarda las URLs en el campo `image`
+      author: req.userId, // Obtener ID del usuario desde el token
+    });
+
+
+    // Populate the author field with user details
+    const populatedPost = await Post.findById(post._id)
+      .populate("author", "_id firstName lastName "); // Populate  the id and  name 
+    
+
+    res.status(201).json({
+      message: "Post created successfully",
+      post: populatedPost,
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error", details: error.message }); 
+  }
+};
+
+const getAllPosts = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+const getPostById = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+};
+
+const updatePostById = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+};
+
+const deletePostById = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+};
+
+export {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePostById,
+  deletePostById
+}
