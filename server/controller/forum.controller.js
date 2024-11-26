28e510ef38e638cd844c6b23c183794d9155e935
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-26 07:53:36 -0800

    feat: :sparkles: add update forum by id

--- server/controller/forum.controller.js
+++ server/controller/forum.controller.js
@@ -17,24 +17,44 @@ const createForum = async (req, res) => {
 };
 
 const getAllForums = async (req, res) => {
+
   try {
+    const forums = await Forum.find({ isActive: true }).populate("posts", "_id title content image authors ");
+    if (!forums) return res.status(404).json({ message: 'No Forums Were Found!' });
 
+    res.status(200).json(forums);
   } catch (error) {
-
+    res.status(400).json({ message: error.message })
   }
 };
 
 const getForumById = async (req, res) => {
+  // check to see if ID is a matching with a matching object(has to be 24  hexadecimal  alphanumeric characters)
+  if (!req.params.forumId.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(400).json({ message: 'Invalid post ID' })
+  }
   try {
+    const forum = await Forum.find({ _id: req.params.forumId, isActive: true }).populate("posts", "_id title content image authors ");
+    if (!forum) return res.status(404).json({ message: 'No Forum Was Found!' });
 
+    res.status(200).json(forum);
   } catch (error) {
-
+    res.status(400).json({ message: error.message })
   }
 };
 
 const updateForumById = async (req, res) => {
-  try {
 
+  // check to see if ID is a matching with a matching object(has to be 24  hexadecimal  alphanumeric characters)
+  if (!req.params.forumId.match(/^[0-9a-fA-F]{24}$/)) {
+    return res.status(400).json({ message: 'Invalid post ID' })
+  }
+  try {
+    const forum = await Forum.findByIdAndUpdate(req.params.forumId, req.body, { new: true }).populate("posts", "_id title content image authors ");
+    
+    if (!forum) return res.status(404).json({ message: 'No Forum Was Found!' });
+
+    res.status(200).json(forum);
   } catch (error) {
 
   }
