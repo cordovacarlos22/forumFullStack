23c31d119837abbe5dc28cce766cd531a623c021
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-25 18:03:54 -0800

    feat: :sparkles: add post path to create a post

---
+++ server/routes/post.route.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import * as postController from '../controller/post.controller.js'
+import { isAuth } from '../middlewares/isAuth.js';
+import { uploadFiles,multerErrorHandler } from '../middlewares/multerUploadFile.midleware.js';
+const postRoute = express.Router();
+
+// create a post with auth validation middleware  to get user id 
+postRoute.post('/post', isAuth,multerErrorHandler(uploadFiles), postController.createPost);
+
+
+export default postRoute
