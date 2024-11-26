60fc9a97d893dbd0da6d55357de307e92175ca2a
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-25 18:01:06 -0800

    fix: :bug: FIX RETURN ON URLS

--- server/config/s3Service.js
+++ server/config/s3Service.js
@@ -8,7 +8,7 @@ dotenv.config(); // initialize dotenv configuration
 // Function to upload a file to S3 using AWS SDK V3
 const s3UploadV3 = async (files) => {
   // instance of S3Client
-  const s3 = new S3Client({ region: process.env.AWS_REGION });
+  const s3Client = new S3Client({ region: process.env.AWS_REGION });
 
   const awsBucketName = process.env.AWS_BUCKET_NAME // aws s3 bucket name 
   // parameters to be passed to aws upload function 
@@ -22,24 +22,21 @@ const s3UploadV3 = async (files) => {
       Body: file.buffer, // multer passes buffer when file is save to memory 
     };
 
-    // returns promise of putObjectCommand
-
-
   });
 
   // awaits all promises to resolve  and Upload all files in parallel
-  const result = await Promise.all(
-    params.map((param) => S3Client.send(new PutObjectCommand(param)))
+  const results = await Promise.all(
+    params.map((param) => s3Client.send(new PutObjectCommand(param)))
   );
 
   // Generate public URLs for all files
-  const urls = params.map((param) => {
-    `https://${param.Bucket}.s3.us-west-1.amazonaws.com/${param.Key}`
-  });
+  const urls = params.map((param) =>
+    `https://${awsBucketName}.s3.us-west-1.amazonaws.com/${param.Key}`
+  );
   return {
-    result,
-    urls, // returns array of public URLs for all uploaded files
-    success: true, // indicates if all files were uploaded successfully
+    results,
+    urls // returns array of public URLs for all uploaded files
+   
 
   }
 
