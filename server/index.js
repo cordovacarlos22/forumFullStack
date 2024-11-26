1cd38d424c9d725f71a1a959b37775dcbbb40f24
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-26 07:34:53 -0800

    feat: :sparkles: introduce forum routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { connect } from './config/database.js'
 import userRoutes from './routes/user.route.js'
 import postRoute from './routes/post.route.js'
+import forumRoutes from './routes/forum.route.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -9,8 +10,11 @@ const api = express()
 
 api.use(express.json())
 
-api.use('/api/v1', userRoutes)
-api.use('/api/v1', postRoute)
+api.use('/api/v1', userRoutes);
+
+api.use('/api/v1', postRoute);
+
+api.use('/api/v1', forumRoutes);
 
 connect().then(() => {
     api.listen(PORT, () => {
