70e8fff827549661b4a6c0aed5e6077c5242a34f
Hector Castro <raidrex@hotmail.com>
2024-11-27 18:25:50 -0600

    creacion de middleware para que solo el mismo usuario pueda editar sus datos

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,7 @@ import * as userAuth from '../controller/user.auth.controller.js';
 import * as userController from '../controller/user.controller.js';
 import { isAuth } from '../middlewares/isAuth.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
+import { isTheSameUser } from '../middlewares/isTheSameUser.js';
 
 
 const userRoutes = express.Router();
@@ -11,7 +12,7 @@ userRoutes.post('/user/register', userAuth.register); // register user
 userRoutes.post('/user/login', userAuth.login); // login user 
 userRoutes.get('/user', isAuth, isAdmin, userController.getAllUser); // get all user by admin only 
 userRoutes.get('/user/:userId', isAuth, isAdmin, userController.getUserById);// get user by id user most by auth
-userRoutes.patch('/user/:userId', isAuth, userController.updateUserById) // update user by id 
+userRoutes.patch('/user/:userId', isAuth, isTheSameUser, userController.updateUserById) // update user by id 
 userRoutes.delete('/user/:userId', isAuth,isAdmin, userController.deleteUserById) // update user by id 
 
 export default userRoutes;
