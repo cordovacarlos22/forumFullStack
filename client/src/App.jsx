62e241efc8a43f33851595b3793c60953a9d0b00
Hector Castro <raidrex@hotmail.com>
2024-11-29 18:58:15 -0600

    creada la page de register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Nav from "./components/Nav"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
+import Register from './pages/Register'
 
 export default function App() {
   return (
    <>
-   <Login/>
+   <Register/>
 
    </>
   )
