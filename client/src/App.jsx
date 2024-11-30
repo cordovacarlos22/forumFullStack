ed270cc970ce903d994b65f65e30874222ab1a5d
Hector Castro <raidrex@hotmail.com>
2024-11-30 15:02:32 -0600

    correcciones menores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import Nav from "./components/Nav"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from './pages/Register'
-import { AuthProvider } from "./context/authContext"
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { AuthProvider } from "./context/authContext";
 
 export default function App() {
   return (
-   <>
-   <AuthProvider>
-   <Login/>
-
-   </AuthProvider>
-
-   </>
-  )
+    <>
+      <AuthProvider>
+        {/* <Login/> */}
+        <Register />
+      </AuthProvider>
+    </>
+  );
 }
