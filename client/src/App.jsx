c2324fcd30c4e9ef70e9b56699821831b818ffd5
Hector Castro <raidrex@hotmail.com>
2024-11-29 19:52:28 -0600

    creado contexto de autenticacion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Register from './pages/Register'
 export default function App() {
   return (
    <>
-   <Register/>
+   <Login/>
 
    </>
   )
---
+++ client/src/context/authContext.js
@@ -0,0 +1,47 @@
+import { jwtDecode } from 'jwt-decode'
+import {createContext, useState, useEffect} from 'react'
+
+const AuthContext = createContext()
+
+const AuthProvider = ({ children }) => {
+    const [autenticated, setAutenticated] = useState(false)
+    const [userPayload, setUserPayload] = useState(null)
+
+    const login = (jwtToken) => {
+        localStorage.setItem('token', jwtToken)
+        const payload = jwtDecode(jwtToken)
+        setAutenticated(true)
+        setUserPayload(payload)
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        setAutenticated(false)
+        setUserPayload(null)
+    }
+
+    useEffect( ()=> {
+        const token = localStorage.getItem('token')
+        if(token) {
+            const payload = jwtDecode(token)
+            setAutenticated(true)
+            setUserPayload(payload)
+        }
+    },[])
+
+    const data = {
+        autenticated,
+        userPayload,
+        login,
+        logout,
+    }
+
+    return (
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+
+}
+export {AuthContext, AuthProvider}
