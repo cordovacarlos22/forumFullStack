275841697dd327458265d582f11160cdec00b9af
Hector Castro <raidrex@hotmail.com>
2024-11-29 18:04:23 -0600

    agregado flowbite react y creacion de la pagina de login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
+import Nav from "./components/Nav"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <>
+   <Login/>
+
+   </>
   )
 }
