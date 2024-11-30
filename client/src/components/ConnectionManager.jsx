12ffb80ce569d11a7fcd4f1ca627e51a1faf7726
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-30 07:28:08 -0800

    feat: :sparkles: add boiler plate components for chat

---
+++ client/src/components/ConnectionManager.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConnectionManager = () => {
+  return (
+    <div>ConnectionManager</div>
+  )
+}
+
+export default ConnectionManager
---
+++ client/src/components/ConnectionState.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConnectionState = () => {
+  return (
+    <div>ConnectionState</div>
+  )
+}
+
+export default ConnectionState
