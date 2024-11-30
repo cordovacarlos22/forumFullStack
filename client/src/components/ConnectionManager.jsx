d90f242b81ae9842934bf7ec49aef91bb8344d77
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-30 08:50:21 -0800

    feat: :sparkles: introduce connetcion manager component

--- client/src/components/ConnectionManager.jsx
+++ client/src/components/ConnectionManager.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
 
-const ConnectionManager = () => {
+import { socket } from '../utils/socket';
+
+
+
+
+const ConnectionManager = ({ isConnected }) => {
+
+  const handleConnect = () => {
+    socket.connect();
+  }
+
+  const handleDisconnect = () => {
+    socket.disconnect();
+  }
   return (
-    <div>ConnectionManager</div>
+    <>
+      {isConnected ? (
+        <>
+          <button
+            onClick={handleDisconnect}
+          >Disconect Chat</button>
+        </>
+      ) : (
+        <>
+          <button
+            onClick={handleConnect}
+          >Connect Chat</button >
+        </>
+
+      )}
+
+
+    </>
   )
 }
 
