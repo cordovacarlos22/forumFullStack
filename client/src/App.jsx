e9680f02844c932f37d331e485c61b84b534e6a6
manuel <mfmoreno092000@gmail.com>
2024-11-30 12:45:54 -0600

    se crearon las rutas de inicio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import ErrorPage from './pages/Errorpage'
+
+ function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Nav></Nav>,
+
+      errorElement: <ErrorPage />,
+      children: [
+        { path: '/', element: <Home /> },
+  /*    { path: '/', element: <Login /> },
+        { path: '/', element: <Register /> },
+        { path: '/CreatePosts', element: <CreatePosts /> },
+        { path: '/PostsProfile', element: <PostsProfile /> },
+        { path: '/Profile', element: <Profile /> }, 
+        { path: '/PostsComments', element: <PostsComments /> }, */
+      ],
+    }
+  ])
+
+
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <RouterProvider router={router}/>
+    </>
   )
 }
+
+export default App
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+  <div>
+    Nav
+    <Outlet></Outlet>
+  </div>
   )
 }
 
