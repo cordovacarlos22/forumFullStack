ca52a70050b2b51b2674b1437ff48d9d252c9b50
ErickPonceLIV <erickponce2093@gmail.com>
2024-12-02 10:29:50 -0600

    update Nav.jsx add new icon and refactor elements

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,21 +4,23 @@ const Nav = () => {
 <nav className="max-w-screen bg-white border-gray-200 dark:bg-gray-900">
   <div className="flex justify-between p-4">
     <div className="flex items-center space-x-3 rtl:space-x-reverse">
-      <svg
-        className="w-[46px] h-[46px] text-gray-800 dark:text-white"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width={24}
-        height={24}
-        fill="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          fillRule="evenodd"
-          d="M8.64 4.737A7.97 7.97 0 0 1 12 4a7.997 7.997 0 0 1 6.933 4.006h-.738c-.65 0-1.177.25-1.177.9 0 .33 0 2.04-2.026 2.008-1.972 0-1.972-1.732-1.972-2.008 0-1.429-.787-1.65-1.752-1.923-.374-.105-.774-.218-1.166-.411-1.004-.497-1.347-1.183-1.461-1.835ZM6 4a10.06 10.06 0 0 0-2.812 3.27A9.956 9.956 0 0 0 2 12c0 5.289 4.106 9.619 9.304 9.976l.054.004a10.12 10.12 0 0 0 1.155.007h.002a10.024 10.024 0 0 0 1.5-.19 9.925 9.925 0 0 0 2.259-.754 10.041 10.041 0 0 0 4.987-5.263A9.917 9.917 0 0 0 22 12a10.025 10.025 0 0 0-.315-2.5A10.001 10.001 0 0 0 12 2a9.964 9.964 0 0 0-6 2Zm13.372 11.113a2.575 2.575 0 0 0-.75-.112h-.217A3.405 3.405 0 0 0 15 18.405v1.014a8.027 8.027 0 0 0 4.372-4.307ZM12.114 20H12A8 8 0 0 1 5.1 7.95c.95.541 1.421 1.537 1.835 2.415.209.441.403.853.637 1.162.54.712 1.063 1.019 1.591 1.328.52.305 1.047.613 1.6 1.316 1.44 1.825 1.419 4.366 1.35 5.828Z"
-          clipRule="evenodd"
-        />
-      </svg>
+    <svg
+  className="w-[48px] h-[48px] text-gray-800 dark:text-white"
+  aria-hidden="true"
+  xmlns="http://www.w3.org/2000/svg"
+  width={24}
+  height={24}
+  fill="none"
+  viewBox="0 0 24 24"
+>
+  <path
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeWidth={2}
+    d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+  />
+</svg>
+
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
         MushRoom-Forum
       </span>
@@ -69,7 +71,7 @@ const Nav = () => {
     <div className="flex items-center space-x-6 rtl:space-x-reverse">
         <button
         type="button"
-        className="text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2"
+        className=" text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2"
       >
         Iniciar Sesión
       </button>
@@ -81,12 +83,22 @@ const Nav = () => {
         data-dropdown-toggle="user-dropdown"
         data-dropdown-placement="bottom"
       >
-        <span className="sr-only">Open user menu</span>
-        <img
-          className="w-8 h-8 rounded-full"
-          src="/docs/images/people/profile-picture-3.jpg"
-          alt="user photo"
-        />
+        <span className=" sr-only">Open user menu</span>
+        <svg
+  className="w-[41px] h-[41px] text-gray-800 dark:text-white"
+  aria-hidden="true"
+  xmlns="http://www.w3.org/2000/svg"
+  width={24}
+  height={24}
+  fill="currentColor"
+  viewBox="0 0 24 24"
+>
+  <path
+    fillRule="evenodd"
+    d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+    clipRule="evenodd"
+  />
+</svg>
       </button>
     </div>
   </div>
