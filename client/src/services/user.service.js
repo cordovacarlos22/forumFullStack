2b4b43df48217e1cb9b829004be06eb0e3f3c40a
Hector Castro <raidrex@hotmail.com>
2024-12-03 21:23:25 -0600

    creacion de servicio para renderizar la data del usuario logueado

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const mainUrl = /* import.meta.env.VITE_FORO_API ||  */'http://localhost:3000/api/v1'
 
-const getMyUserService = (token, userId) => axios.get(`${mainUrl}/users/${userId}`, {
+const getMyUserService = (token, userId) => axios.get(`${mainUrl}/user/${userId}`, {
     headers: {
         Authorization: `Bearer ${token}`
     }
