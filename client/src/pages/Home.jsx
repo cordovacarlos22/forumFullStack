99078a7c184a7c34fec6048263ea999d30f35723
Hector Castro <raidrex@hotmail.com>
2024-12-03 22:48:07 -0600

    creada peticion para agregar post a home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,26 @@
 import { useAuthContext } from "../hooks/useAuth";
+import { useState, useEffect } from "react";
+import { getPostsService } from "../services/post.service";
 
 const Home = () => {
   const { userPayload, autenticated } = useAuthContext(); // token viene del contexto
+  const [ posts, setPosts ] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await getPostsService()
+        console.log(response.data);
+        
+        setPosts(response.data)
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+        
+      }
+    }
+
+    fetchPosts()
+  }, [])
 
   return (
     <div>
@@ -14,6 +33,11 @@ const Home = () => {
       ) : (
         <p>Loading user info...</p>
       )}
+      <div>
+      {posts.map((post) => {
+        <div key={post._id}>{post.title}{post.content}{post.image}</div>
+      })}
+      </div>
     </div>
   );
 };
