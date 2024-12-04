f598d2bac3d60f2be4243ac8aed42d036ee9687b
carloscordova.dev <cordovamanuel22@icloud.com>
2024-12-03 18:17:06 -0800

    feat: :sparkles: add route to get all comments

--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -4,7 +4,13 @@ import * as commentController from '../controller/comment.controller.js'
 // Import routes
 const commentRoutes = express.Router();
 
-commentRoutes.post('/comments/post/:postId',isAuth, commentController.createComment);
+commentRoutes.post('/comments/post/:postId', isAuth, commentController.createComment);
+
+// Get all comments for a post
+commentRoutes.get('/comments', commentController.getAllComments);
+// commentRoutes.get('/comments/post/:postId', commentController.getAllCommentsByPost);
+// update comments is auth y is the  same user
+// delete comments is auth y is the same user
 
 
 export default commentRoutes
