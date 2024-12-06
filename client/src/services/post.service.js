50c134c5e7ba5a03c642993f0aca436b31fd659c
carloscordova.dev <cordovamanuel22@icloud.com>
2024-12-05 18:30:18 -0800

    feat: :sparkles: removed comment on line 3

--- client/src/services/post.service.js
+++ client/src/services/post.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const mainUrl = /* import.meta.env.VITE_FORO_API ||  */'http://localhost:3000/api/v1'
+const mainUrl = import.meta.env.VITE_FORO_API || 'http://localhost:3000/api/v1'
 
 const getPostsService = () => axios.get(`${mainUrl}/post`)
 
