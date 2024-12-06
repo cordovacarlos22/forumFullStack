46781762564e6419e1ba384ab839a0c9b872638d
manuel <mfmoreno092000@gmail.com>
2024-12-05 19:06:06 -0600

    agrege los estilos de post y comments👷

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom-glass': '0 4px 30px rgba(0, 0, 0, 0.1)', // Box shadow
+      },
+      backdropBlur: {
+        'custom': '18.8px', // Backdrop blur
+      },
+      colors: {
+        'glass-bg': 'rgba(255, 255, 255, 0.28)', // Semi-transparent background
+        'glass-border': 'rgba(255, 255, 255, 0.7)', // Border color
+      },
+      borderRadius: {
+        'glass': '16px', // Border radius
+      },
+    },
   },
   plugins: [],
 }
