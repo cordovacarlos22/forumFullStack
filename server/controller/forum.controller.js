6b8e65a9fed5076c5708d6e96596fe5320c61a95
carloscordova.dev <cordovamanuel22@icloud.com>
2024-12-06 16:44:54 -0800

    feat: :sparkles: add populate on get forum by id and update forum by id

--- server/controller/forum.controller.js
+++ server/controller/forum.controller.js
@@ -42,7 +42,14 @@ const getForumById = async (req, res) => {
     return res.status(400).json({ message: 'Invalid post ID' })
   }
   try {
-    const forum = await Forum.find({ _id: req.params.forumId, isActive: true }).populate("posts", "_id title content image authors ");
+    const forum = await Forum.find({ _id: req.params.forumId, isActive: true }).populate({
+      path: "posts",
+      select: "_id title content image author",
+      populate: {
+        path: "author", // Populate the author field within posts
+        select: "firstName lastName email avatar", // Specify the fields to include from the author collection
+      },
+    });
     if (!forum) return res.status(404).json({ message: 'No Forum Was Found!' });
 
     res.status(200).json(forum);
@@ -58,8 +65,14 @@ const updateForumById = async (req, res) => {
     return res.status(400).json({ message: 'Invalid post ID' })
   }
   try {
-    const forum = await Forum.findByIdAndUpdate(req.params.forumId, req.body, { new: true }).populate("posts", "_id title content image authors ");
-
+    const forum = await Forum.findByIdAndUpdate(req.params.forumId, req.body, { new: true }).populate({
+      path: "posts",
+      select: "_id title content image author",
+      populate: {
+        path: "author", // Populate the author field within posts
+        select: "firstName lastName email avatar", // Specify the fields to include from the author collection
+      },
+    });
     if (!forum) return res.status(404).json({ message: 'No Forum Was Found!' });
 
     res.status(200).json(forum);
