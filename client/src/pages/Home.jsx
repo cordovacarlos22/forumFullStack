5cabef25fdd430d30a48a983c68b5a7e8391f397
carloscordova.dev <cordovamanuel22@icloud.com>
2024-12-07 12:09:30 -0800

    feat: :sparkles: rendering spinner with condition

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,58 +3,62 @@ import { useContext } from "react";
 import { ForumContext } from "../context/forum.context";
 import Post from "../components/Post";
 import Aside from "../components/Aside";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Home = () => {
-  const { userPayload, autenticated } = useAuthContext(); // token viene del contexto
-  const { forums } = useContext(ForumContext);
+  const { userPayload, autenticated } = useAuthContext();
+  const { filteredForums, loading } = useContext(ForumContext);
 
   return (
-    <div className="md:flex w-screen min-h-screen bg-gray-900">
-      {/* Aside Section */}
-      <aside className="min-h-screen w-64 bg-gray-800">
-        <Aside />
-      </aside>
+    loading ? (
+      <article className="relative w-screen">
+        <LoadingSpinner />
+      </article>
+    ) : (
+      <>
+        {/* Main Content */}
+        <main className="flex-1 text-white flex flex-col bg-gray-900 mt-8 justify-center items-center">
+          {/* Header */}
+          <header className="p-4">
+            <h1 className="text-2xl font-bold">Home</h1>
+          </header>
 
-      {/* Main Content */}
-      <main className="flex-1 text-white flex flex-col bg-gray-900">
-        {/* Header */}
-        <header className="p-4">
-          <h1 className="text-2xl font-bold">Home</h1>
-        </header>
+          {/* User Info */}
+          <section className="p-4">
+            {autenticated && (
+              <div>
+                <h2>
+                  Welcome, {userPayload.firstName} {userPayload.lastName}
+                </h2>
+              </div>
+            )}
+          </section>
 
-        {/* User Info */}
-        <section className="p-4">
-          {autenticated ? (
-            <div>
-              <p>Name: {userPayload.firstName}</p>
-              <p>Email: {userPayload.email}</p>
-            </div>
-          ) : (
-            <p>Loading user info...</p>
-          )}
-        </section>
-
-        {/* Forum Posts */}
-        <section className="p-4 space-y-4">
-          {forums && forums.length > 0 ? (
-            forums.map((forum) =>
-              forum.posts.map((post) => (
-                <Post
-                  key={post._id}
-                  id={post._id}
-                  title={post.title}
-                  forum={forum.title} // Verify if this should be "title"
-                  content={post.content}
-                  postImage={post.image[0]}
-                />
-              ))
-            )
-          ) : (
-            <p>No forums or posts available</p>
-          )}
-        </section>
-      </main>
-    </div>
+          {/* Forum Posts */}
+          <section className="p-4 space-y-4">
+            {filteredForums.length > 0 ? (
+              filteredForums.map((forum) =>
+                forum.posts.map((post) => (
+                  <Post
+                    key={post._id}
+                    id={post._id}
+                    title={post.title}
+                    forum={forum.title}
+                    content={post.content}
+                    postImage={post.image[0]}
+                  />
+                ))
+              )
+            ) : (
+              <main className=" flex justify-center items-center h-screen">
+                    <p>No forums or posts available for this category</p>
+                    <Aside/>
+              </main>
+            )}
+          </section>
+        </main>
+      </>
+    )
   );
 };
 
