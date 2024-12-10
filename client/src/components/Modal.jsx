01bf8a94793dd11a1e4c5ec73a6af0ded4f68f9d
Hector Castro <raidrex@hotmail.com>
2024-12-09 18:10:36 -0600

    renderizado condicional del modal y funcionalidad de botones para cerrarlo

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
-const Modal = ( { setModalOpen, selectedButton } ) => {
+const Modal = ( { setModalOpen, selectedButton, setSelectedButton } ) => {
   const [password, setPassword] = useState('');
 
 
   return (
-    
-    <div className="fixed inset-0 bg-white-700 bg-opacity-50 flex justify-center items-center z-50">
+    <>
+    {selectedButton === 'delete' && (
+
+    <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-gray-800 rounded-lg shadow-lg p-6 w-96">
         <h2 className="text-lg font-bold mb-4 flex justify-center">
             Are you sure?
@@ -36,23 +38,47 @@ const Modal = ( { setModalOpen, selectedButton } ) => {
             <button
               type="button"
               className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
-              onClick={() => setModalOpen(false)}
+              onClick={() => {
+                setModalOpen(false)
+                setSelectedButton('overview')}} 
             >
-              Cancelar
+              Cancel
             </button>
             <button
               type="submit"
-              className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ${
-                password.length === 0 ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
+              className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 
+                ${password.length === 0 ? 'opacity-50 cursor-not-allowed' : '' }`
+              }
               disabled={password.length === 0}
             >
-              Confirmar
+              Comfirm
             </button>
           </div>
         </form>
       </div>
     </div>
+    )}
+
+    {selectedButton === "edit" && (
+
+        <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-gray-800 text-white rounded-lg shadow-lg p-6 w-96">
+            <h2 className="text-lg font-bold mb-4 text-center">Edit Profile</h2>
+            <p className="mb-2 text-center">Functionality under construction.</p>
+            <button
+              type="button"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+              onClick={() => {
+                setModalOpen(false)
+                setSelectedButton('overview')}}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+
+    </>
   );
 };
 
