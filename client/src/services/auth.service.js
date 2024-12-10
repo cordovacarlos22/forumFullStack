a96aef87e7b089d882eda6be4583e6ea575009c8
Hector Castro <raidrex@hotmail.com>
2024-12-10 17:58:39 -0600

    creado el servicio para borrar la cuenta y para cambiar la contraseña

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -2,16 +2,34 @@ import axios from "axios";
 
 const mainUrl = import.meta.env.VITE_FORO_API
 
-const userLogin = (loginData) => axios.post(`${mainUrl}/user/login`, loginData)
+const userLogin = (loginData) => axios.post(`${mainUrl}/user/login`, loginData);
 
-const userRegister = (registerData) => axios.post(`${mainUrl}/user/register`, registerData)
+const userRegister = (registerData) =>
+  axios.post(`${mainUrl}/user/register`, registerData);
 
-export {
-    userLogin,
-    userRegister,
-}
+const deleteUserByPassword = (password, userId, token) => {
+  return axios.delete(`${mainUrl}/user/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    data: { password }, // La contraseña se envía en el cuerpo de la solicitud
+  });
+};
+
+const updateUserPassword = (data, userId, token) => {
+  return axios.put(
+    `${mainUrl}/user/${userId}`,
+    {
+      CurrentPassword: data.password,
+      NewPassword: data.newPassword,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
 
 
-
-
-
+export { userLogin, userRegister, deleteUserByPassword, updateUserPassword };
