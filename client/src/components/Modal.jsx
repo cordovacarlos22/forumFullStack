2fa4c7ef06c24a7289fe379b0342eefb54911d10
Hector Castro <raidrex@hotmail.com>
2024-12-10 18:00:23 -0600

    creada lalogica en el form para utilizar el form de cambio de contraseña, editar perfil queda pendiente

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,6 +1,9 @@
 import { useForm } from "react-hook-form";
 import { useAuthContext } from "../hooks/useAuth";
-import { deleteUserByPassword } from "../services/auth.service";
+import {
+  deleteUserByPassword,
+  updateUserPassword,
+} from "../services/auth.service";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
@@ -10,6 +13,7 @@ import "react-toastify/dist/ReactToastify.css";
 const schema = yup
   .object({
     password: yup.string().min(1).required(),
+    newPassword: yup.string().min(3),
   })
   .required();
 
@@ -22,6 +26,8 @@ const Modal = ({ setModalOpen, selectedButton, setSelectedButton, user }) => {
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
+
+  console.log(errors);
 
   const handleDeleteAccount = async (data) => {
     try {
@@ -45,57 +51,27 @@ const Modal = ({ setModalOpen, selectedButton, setSelectedButton, user }) => {
     }
   };
 
+  const handleUpdatePassword = async (data) => {
+    try {
+      const response = await updateUserPassword(data, user._id, token);
+
+      if (response.status === 200) {
+        toast.success("Password changed successfully, please log in again.");
+        setModalOpen(false);
+        setSelectedButton("overview");
+        logout();
+        navigate("/login");
+      }
+    } catch (error) {
+      const message =
+        error.response?.data?.message ||
+        "Error occurred while changing password";
+      toast.error(message);
+    }
+  };
+
   return (
     <>
-      {selectedButton === "delete" && (
-        <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-gray-800 rounded-lg shadow-lg p-6 w-96">
-            <h2 className="text-lg font-bold mb-4 flex justify-center">
-              Are you sure?
-            </h2>
-            <p>Your account will be permanently deleted.</p>
-            <p>Please enter your password to confirm.</p>
-
-            {/* Formulario */}
-            <form onSubmit={handleSubmit(handleDeleteAccount)}>
-              <label className="block mb-2 text-m font-medium text-white pt-3">
-                Password
-              </label>
-              <input
-                type="password"
-                className="w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 mb-4 text-black"
-                {...register("password")}
-              />
-              {errors.password && (
-                <span className="text-red-500 text-sm">
-                  {errors.password.message}
-                </span>
-              )}
-
-              {/* Botones */}
-              <div className="flex justify-end space-x-3">
-                <button
-                  type="button"
-                  className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
-                  onClick={() => {
-                    setModalOpen(false);
-                    setSelectedButton("overview");
-                  }}
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                >
-                  Confirm
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      )}
-
       {selectedButton === "edit" && (
         <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-gray-800 text-white rounded-lg shadow-lg p-6 w-96">
@@ -195,9 +171,79 @@ const Modal = ({ setModalOpen, selectedButton, setSelectedButton, user }) => {
             <h2 className="text-lg font-bold mb-4 text-center">
               Change Password
             </h2>
-            <form>
+            <form onSubmit={handleSubmit(handleUpdatePassword)}>
               <label className="block mb-2 text-m font-medium text-white pt-3">
                 Current Password
+              </label>
+              <input
+                type="password"
+                className="w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 mb-4 text-black"
+                {...register("password", {
+                  required: "Current password is required",
+                })}
+              />
+              {errors.password && (
+                <span className="text-red-500 text-sm">
+                  {errors.password.message}
+                </span>
+              )}
+
+              <label className="block mb-2 text-m font-medium text-white pt-3">
+                New Password
+              </label>
+              <input
+                type="password"
+                className="w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 mb-4 text-black"
+                autoComplete="off"
+                {...register("newPassword", {
+                  required: "New password is required",
+                  minLength: {
+                    value: 1,
+                    message: "Password is required",
+                  },
+                })}
+              />
+              {errors.newPassword && (
+                <span className="text-red-500 text-sm">
+                  {errors.newPassword.message}
+                </span>
+              )}
+              <div className="flex justify-end space-x-3">
+                <button
+                  type="button"
+                  className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
+                  onClick={() => {
+                    setModalOpen(false);
+                    setSelectedButton("overview");
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                >
+                  Confirm
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {selectedButton === "delete" && (
+        <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-gray-800 rounded-lg shadow-lg p-6 w-96">
+            <h2 className="text-lg font-bold mb-4 flex justify-center">
+              Are you sure?
+            </h2>
+            <p>Your account will be permanently deleted.</p>
+            <p>Please enter your password to confirm.</p>
+
+            {/* Formulario */}
+            <form onSubmit={handleSubmit(handleDeleteAccount)}>
+              <label className="block mb-2 text-m font-medium text-white pt-3">
+                Password
               </label>
               <input
                 type="password"
@@ -210,20 +256,7 @@ const Modal = ({ setModalOpen, selectedButton, setSelectedButton, user }) => {
                 </span>
               )}
 
-              <label className="block mb-2 text-m font-medium text-white pt-3">
-                 New Password
-              </label>
-              <input
-                type="password"
-                className="w-full border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 mb-4 text-black"
-                autoComplete="off"
-                {...register("password")}
-              />
-              {errors.password && (
-                <span className="text-red-500 text-sm">
-                  {errors.password.message}
-                </span>
-              )}
+              {/* Botones */}
               <div className="flex justify-end space-x-3">
                 <button
                   type="button"
